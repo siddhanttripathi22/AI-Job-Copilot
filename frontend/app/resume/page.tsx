5f8263636b168579cf5e4401844948fa74b017fc
Siddhant Tripathi <86283827+siddhanttripathi22@users.noreply.github.com>
2026-06-26 12:35:37 +0530

    Update resume upload API URL to use environment variable

--- frontend/app/resume/page.tsx
+++ frontend/app/resume/page.tsx
@@ -16,7 +16,7 @@ export default function ResumePage() {
 
     try {
       const res = await axios.post(
-        "http://127.0.0.1:8000/resume/upload",
+       `${process.env.NEXT_PUBLIC_API_URL}/resume/upload`,
         formData
       );
       setResult(res.data.data);
